97ccfeda2391eb0e31aa297c5e56cdef9fe2a01b
marialeon-sils <melon@ad.unc.edu>
2024-09-04 21:59:17 -0400

    Add files via upload

---
+++ assignment_01/hello_world.py
@@ -0,0 +1,20 @@
+'''
+This is
+a multiline
+comment
+'''
+# print on one line
+print('hello world')
+
+# print on many lines
+print (
+'''
+<p>this is another paragraoh</p>
+
+
+
+<p>this is another paragraoh</p>
+'''
+
+
+)                                                               
